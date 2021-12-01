43f4254a8a32fd8be4e5d431e5fed0ff9b80a92f
Sheldon Juncker <sheldonjuncker@gmail.com>
2021-12-01 11:12:33 -0500

    Pruning with vectors.

--- src/main/java/com/dreamcloud/esa/indexer/IndexPruner.java
+++ src/main/java/com/dreamcloud/esa/indexer/IndexPruner.java
@@ -4,20 +4,63 @@ import com.dreamcloud.esa.analyzer.AnalyzerOptions;
 import com.dreamcloud.esa.analyzer.EsaAnalyzer;
 import com.dreamcloud.esa.analyzer.TokenizerFactory;
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StoredField;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.PayloadAttribute;
+import org.apache.lucene.document.*;
 import org.apache.lucene.index.*;
+import org.apache.lucene.queryparser.classic.Token;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.util.ArrayList;
+
+class LuceneToken {
+    char[] term;
+    float score;
+}
+
+class CachingTokenStream extends TokenStream {
+
+    private int i = -1;
+    private final ArrayList<LuceneToken> queue;
+
+    private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+    private final PayloadAttribute payloadAtt = addAttribute(PayloadAttribute.class);
+
+    public CachingTokenStream() {
+        this.queue = new ArrayList<>();
+    }
+
+    void produceToken(LuceneToken token) {
+        queue.add(token);
+    }
+
+    public boolean incrementToken() throws IOException {
+        i++;
+        if (queue.size() <= i) {
+            return false;
+        }
+        final LuceneToken token = queue.get(i);
+        int tokenLength = token.term.length;
+        termAtt.resizeBuffer(Math.max(termAtt.buffer().length, tokenLength));
+        termAtt.setLength(tokenLength);
+        final char[] buffer = termAtt.buffer();
+        System.arraycopy(token.term, 0, buffer, 0, tokenLength);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(Float.BYTES);
+        byteBuffer.putFloat(token.score);
+        payloadAtt.setPayload(new BytesRef(byteBuffer.array()));
+        return true;
+    }
+}
 
 public class IndexPruner {
     protected int windowSize;
@@ -49,18 +92,28 @@ public class IndexPruner {
                 System.out.println("leaf: " + l);
                 TermsEnum terms = termDocReader.leaves().get(l).reader().terms("text").iterator();
                 for (BytesRef bytesRef = terms.term(); terms.next() != null; ) {
+                    CachingTokenStream cachingTokenStream = new CachingTokenStream();
                     System.out.println("term: " + bytesRef.utf8ToString());
                     TopDocs td = SearchTerm(bytesRef, docSearcher);
                     Document doc = new Document();
-                    doc.add(new TextField("text", bytesRef.utf8ToString(), Field.Store.NO));
+                    doc.add(new StringField("text", bytesRef.utf8ToString(), Field.Store.NO));
                     for (ScoreDoc scoreDoc: td.scoreDocs) {
                         Document termDocDocument = termDocReader.document(scoreDoc.doc);
-                        String title = termDocDocument.get("title");
-                        String id = termDocDocument.get(DreamIndexer.ID_FIELD);
-                        doc.add(new StoredField("ids", id));
-                        doc.add(new StoredField("names", title));
-                        doc.add(new StoredField("weights", scoreDoc.score));
+                        PostingsEnum postingsEnum;
+                        LuceneToken token = new LuceneToken();
+                        token.term = termDocDocument.get("title").toCharArray();
+                        token.score = scoreDoc.score;
+                        cachingTokenStream.produceToken(token);
                     }
+
+                    FieldType type = new FieldType();
+                    type.setStoreTermVectors(true);
+                    type.setStoreTermVectorPositions(true);
+                    type.setStoreTermVectorPayloads(true);
+                    type.setStored(false);
+                    type.setTokenized(true);
+                    type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS);
+                    doc.add(new Field("concept", cachingTokenStream, type));
                     conceptIndexWriter.addDocument(doc);
                 }
             }
