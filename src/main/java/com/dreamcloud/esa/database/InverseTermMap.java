7416a30798d327546a971f2e59dcb802833f0eae
Sheldon Juncker <sheldonjuncker@gmail.com>
2021-12-06 12:28:58 +0000

    Switched to binary UUID index.

--- src/main/java/com/dreamcloud/esa/database/InverseTermMap.java
+++ src/main/java/com/dreamcloud/esa/database/InverseTermMap.java
@@ -1,6 +1,7 @@
 package com.dreamcloud.esa.database;
 
 import java.sql.*;
+import java.util.UUID;
 
 public class InverseTermMap {
     PreparedStatement statement;
@@ -10,6 +11,34 @@ public class InverseTermMap {
         this.con = con;
     }
 
+    protected static byte hexToByte(String hexString) {
+        int firstDigit = toDigit(hexString.charAt(0));
+        int secondDigit = toDigit(hexString.charAt(1));
+        return (byte) ((firstDigit << 4) + secondDigit);
+    }
+
+    protected static int toDigit(char hexChar) {
+        int digit = Character.digit(hexChar, 16);
+        if(digit == -1) {
+            throw new IllegalArgumentException(
+                    "Invalid Hexadecimal Character: "+ hexChar);
+        }
+        return digit;
+    }
+
+    protected static byte[] decodeHexString(String hexString) {
+        if (hexString.length() % 2 == 1) {
+            throw new IllegalArgumentException(
+                    "Invalid hexadecimal String supplied.");
+        }
+
+        byte[] bytes = new byte[hexString.length() / 2];
+        for (int i = 0; i < hexString.length(); i += 2) {
+            bytes[i / 2] = hexToByte(hexString.substring(i, i + 2));
+        }
+        return bytes;
+    }
+
     protected PreparedStatement getStatement() throws SQLException {
         if (statement == null) {
             statement = con.prepareStatement("insert into dc.term_map(`term_id`, `concept_id`, `score`) values(?, ?, ?)");
@@ -17,23 +46,22 @@ public class InverseTermMap {
         return statement;
     }
 
+    protected static byte[] generateUuid() {
+        UUID id = UUID.randomUUID();
+        return decodeHexString(id.toString().replace("-", ""));
+    }
+
     public void saveTermScores(TermScores termScores) throws SQLException {
         //Create term
-        PreparedStatement termStatement = con.prepareStatement("insert into dc.term(`term`) values(?)", Statement.RETURN_GENERATED_KEYS);
-        termStatement.setString(1, termScores.term.utf8ToString());
+        PreparedStatement termStatement = con.prepareStatement("insert into dc.term(`id`, `term`) values(?, ?)");
+        byte[] id = generateUuid();
+        termStatement.setBytes(1, id);
+        termStatement.setString(2, termScores.term.utf8ToString());
         termStatement.executeUpdate();
-        int termId;
-        ResultSet genKeys = termStatement.getGeneratedKeys();
-        if ( genKeys.next() ) {
-            termId = genKeys.getInt( 1 );
-        } else {
-            throw new SQLException("Failed to fetch generated keys.");
-        }
-        System.out.println("term id: " + termId);
 
         PreparedStatement  statement = getStatement();
         for(TermScore score: termScores.scores) {
-            statement.setInt(1, termId);
+            statement.setBytes(1, id);
             statement.setLong(2, score.docId);
             statement.setFloat(3, score.score);
             statement.executeUpdate();
