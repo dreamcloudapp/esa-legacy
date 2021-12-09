ca59af15dc50906f7175ea880c88f090091f12fd
Sheldon Juncker <sheldonjuncker@gmail.com>
2021-12-09 12:02:15 +0000

    Added separate annotator stage for weighting / repeating titles and links.

--- src/main/java/com/dreamcloud/esa/annoatation/WikiLinkHandler.java
+++ src/main/java/com/dreamcloud/esa/annoatation/WikiLinkHandler.java
@@ -1,60 +1,36 @@
 package com.dreamcloud.esa.annoatation;
 
-import com.dreamcloud.esa.annoatation.handler.XmlWritingHandler;
+import com.dreamcloud.esa.annoatation.handler.XmlReadingHandler;
 import com.dreamcloud.esa.tools.StringUtils;
 
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.stream.XMLStreamException;
-import java.io.File;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class WikiLinkHandler extends XmlWritingHandler {
-    protected final SAXParserFactory saxFactory;
+public class WikiLinkHandler extends XmlReadingHandler {
     protected int numStripped = 0;
     static Pattern linkRegexPattern = Pattern.compile("\\[\\[(?!File:|Image:)([^|#\\]]+)[^]]*]]");
     protected Map<String, String> titleMap;
     protected Map<String, WikiLinkAnnotation> annotations;
 
-    public WikiLinkHandler(Map<String, String> titleMap, Map<String, WikiLinkAnnotation> annotations, File outputFile) throws XMLStreamException, IOException {
+    public WikiLinkHandler(Map<String, String> titleMap, Map<String, WikiLinkAnnotation> annotations) {
         this.titleMap = titleMap;
         this.annotations = annotations;
-        saxFactory = SAXParserFactory.newInstance();
-        saxFactory.setNamespaceAware(true);
-        saxFactory.setValidating(false);
-        saxFactory.setXIncludeAware(true);
-        this.open(outputFile);
-        this.writeDocumentBegin("docs");
     }
 
     public void handleDocument(Map<String, String> xmlFields) {
         String title = xmlFields.get("title");
-        StringBuilder text = new StringBuilder(xmlFields.get("text"));
-        Matcher matcher = linkRegexPattern.matcher(text.toString());
+        String text = xmlFields.get("text");
+        Matcher matcher = linkRegexPattern.matcher(text);
         Set<String> outgoingLinks = new HashSet<>();
         while (matcher.find()) {
             String normalizedLink = StringUtils.normalizeWikiTitle(matcher.group(1));
             if (titleMap.containsKey(normalizedLink)) {
-                text.append((" " + normalizedLink).repeat(3));
                 outgoingLinks.add(titleMap.get(normalizedLink));
             }
         }
-        text.append((" " + title).repeat(4));
-
-        try {
-            this.writeStartElement("doc");
-            this.writeElement("title", title);
-            this.writeElement("text", text.toString());
-            this.writeEndElement();
-        } catch (XMLStreamException|IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-
 
         //Handle outgoing links and token count
         if (!annotations.containsKey(title)) {
@@ -81,11 +57,5 @@ public class WikiLinkHandler extends XmlWritingHandler {
         if (this.getDocsRead() % 1000 == 0) {
             System.out.println("link-annotated article\t[" + this.getDocsRead() + "]\t\"" + title + "\"");
         }
-    }
-
-    @Override
-    public void close() throws Exception {
-        writeDocumentEnd();
-        super.close();
     }
 }
