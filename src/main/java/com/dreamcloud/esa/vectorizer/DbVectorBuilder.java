51a6e9d107f637ff83bfbc3b3c83f43610425f1b
Sheldon Juncker <sheldonjuncker@gmail.com>
2021-12-18 12:25:14 +0000

    Fixed bug with document dimension addition.

--- src/main/java/com/dreamcloud/esa/vectorizer/DbVectorBuilder.java
+++ src/main/java/com/dreamcloud/esa/vectorizer/DbVectorBuilder.java
@@ -7,6 +7,8 @@ import org.apache.lucene.analysis.Analyzer;
 import org.eclipse.collections.api.map.primitive.MutableObjectIntMap;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DbVectorBuilder implements VectorBuilder {
     TfIdfScoreRepository scoreRepository;
@@ -22,19 +24,20 @@ public class DbVectorBuilder implements VectorBuilder {
     public ConceptVector build(String document) throws IOException {
         ConceptVector vector = new ConceptVector();
         TfIdfScore[] scores = tfIdfAnalyzer.getTfIdfScores(document);
+        Map<String, Double> scoreMap = new HashMap<>();
         String []terms = new String[scores.length];
         int i = 0;
         for (TfIdfScore score: scores) {
             terms[i++] = score.getTerm();
+            scoreMap.put(score.getTerm(), score.getScore());
         }
         TfIdfScore[] scoreDocs = scoreRepository.getTfIdfScores(terms);
 
         for (TfIdfScore docScore: scoreDocs) {
             double score = docScore.getScore();
-            for (TfIdfScore termScore: scores) {
-                if (termScore.getTerm().equals(docScore.getTerm())) {
-                    score *= termScore.getScore();
-                }
+            score *= scoreMap.get(docScore.getTerm());
+            if (vector.conceptWeights.containsKey(docScore.getDocument())) {
+                score += vector.conceptWeights.get(docScore.getDocument());
             }
             vector.conceptWeights.put(docScore.getDocument(), (float) score);
         }
