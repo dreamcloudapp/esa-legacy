c1315714a6cd75de3002bfbfa1159e1307fcac51
Sheldon Juncker <sheldonjuncker@gmail.com>
2022-01-18 16:58:16 -0500

    Removed testing with TF.

--- src/main/java/com/dreamcloud/esa/tfidf/strategy/tf/NaturalTermFrequency.java
+++ src/main/java/com/dreamcloud/esa/tfidf/strategy/tf/NaturalTermFrequency.java
@@ -7,8 +7,7 @@ import com.dreamcloud.esa.tfidf.strategy.TermFrequencyStrategy;
  * Normal term frequency (doesn't adjust in any way).
  */
 public class NaturalTermFrequency implements TermFrequencyStrategy {
-    //todo: play around with this as just for querying it makes things slightly better! (0.002 or so)
     public double tf(double tf, TermInfo termInfo) {
-        return Math.pow(tf, 4);
+        return tf;
     }
 }
